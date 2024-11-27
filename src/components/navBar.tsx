c7e00876308da836b8b39acfc6279fa3c163918e
Raine <64136899+raineaeternal@users.noreply.github.com>
2024-11-27 13:56:09 +0100

    fix eslint errors

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -6,7 +6,7 @@ export default function NavBar() {
     return (
         <div className="bg-transparent flex items-center justify-between p-2 shadow-lg">
             <div className="flex items-center">
-                <img className="mr-4 size-16 rounded-lg" src="../../public/FrontPunchIn.png" alt="Image of Raine's avatar" />
+                <img className="mr-4 size-16 rounded-lg" src="/FrontPunchIn.png" alt="Image of Raine's avatar" />
             </div>
             <div className="flex space-x-4 ibm-plex-mono-regular">
                 <div className="hover:text-white rounded-md p-4 btn-primary">
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router';
+import { BrowserRouter } from 'react-router';
 import './index.scss'
 import Home from './App.tsx'
-import FFXIV from './pages/ffxiv.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
