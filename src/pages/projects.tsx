ef13325eede1adbc7d72eb8519511c1c96192b07
Raine <64136899+raineaeternal@users.noreply.github.com>
2024-11-29 10:18:35 +0100

    add more details and fix css

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,12 +1,13 @@
 import "../fonts.scss";
 import "../App.scss"
+import { Link } from "react-router";
 
 export default function Projects() {
     return (
         <>
-            <div className="ml-64 mr-64">
+            <div className="flex flex-col justify-items-start ml-64 mr-64">
 
-                <div className="flex flex-col justify-items-start">
+                <div>
                     <h1 className="text-5xl ibm-plex-mono-regular">
                         Projects
                     </h1>
@@ -28,7 +29,7 @@ export default function Projects() {
                             Role: Backend/Database
                         </p>
 
-                        <p>
+                        <p className="pl-12">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio harum accusantium excepturi, vel vitae maiores nemo ea, ab sint eos totam exercitationem non tempora blanditiis placeat rerum voluptatibus architecto libero.
                             Quaerat ab natus excepturi eos at, veniam alias nulla, commodi, recusandae voluptatum magnam optio voluptatem ipsum nesciunt perspiciatis reiciendis iusto. Quam enim beatae quo ea ducimus incidunt temporibus accusantium doloribus!
                             Iusto voluptate pariatur dolorem facere reprehenderit nihil numquam nostrum ad cumque est laudantium vero corporis dolore, praesentium atque ducimus. Non quo itaque alias magni, odit reprehenderit quod iusto earum voluptatibus?
@@ -36,9 +37,7 @@ export default function Projects() {
                             Quas, itaque ut deleniti corporis sunt illum ipsam hic eos obcaecati cum praesentium labore quibusdam possimus! Ratione consequuntur amet quis commodi enim eum a nam. Ab provident voluptatum nisi cupiditate!
                         </p>
 
-                        <div className="btn-secondary p-4 rounded-l text-black mt-2">
-                            <a href="http://github.com/TournamentTools/">Project Source</a>
-                        </div>
+                        <Link className="btn-secondary p-4 rounded-l text-black mt-2" to="http://github.com/TournamentTools/">Project Source</Link>
                     </div>
                 </section>
 
@@ -47,17 +46,16 @@ export default function Projects() {
                         <h2 className="text-4xl ibm-plex-mono-regular">
                             TournamentTools
                         </h2>
-                        <p className="text- ibm-plex-mono-regular">
+                        <p className="text-sm ibm-plex-mono-regular">
                             Role: System/Flow Engineer
                         </p>
-                        <p>
+                        <p className="pl-12">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio harum accusantium excepturi, vel vitae maiores nemo ea, ab sint eos totam exercitationem non tempora blanditiis placeat rerum voluptatibus architecto libero.
                             Quaerat ab natus excepturi eos at, veniam alias nulla, commodi, recusandae voluptatum magnam optio voluptatem ipsum nesciunt perspiciatis reiciendis iusto. Quam enim beatae quo ea ducimus incidunt temporibus accusantium doloribus!
                             Iusto voluptate pariatur dolorem facere reprehenderit nihil numquam nostrum ad cumque est laudantium vero corporis dolore, praesentium atque ducimus. Non quo itaque alias magni, odit reprehenderit quod iusto earum voluptatibus?
                             Temporibus, aliquam odit. Quidem minus nesciunt aspernatur qui nihil. Aperiam amet dolor commodi, nostrum hic dolorum. Quos dolor consequatur officiis libero neque mollitia ex voluptatum, fuga, facilis quod sunt atque.
                             Quas, itaque ut deleniti corporis sunt illum ipsam hic eos obcaecati cum praesentium labore quibusdam possimus! Ratione consequuntur amet quis commodi enim eum a nam. Ab provident voluptatum nisi cupiditate!
                         </p>
-
                     </div>
                 </section>
 
@@ -66,16 +64,19 @@ export default function Projects() {
                         <h2 className="text-4xl ibm-plex-mono-regular">
                             SaberQuest
                         </h2>
-                        <p className="text- ibm-plex-mono-regular">
+                        <p className="text-sm ibm-plex-mono-regular">
                             Role: Full Stack (Backend focus)
+                            Stack: Express TypeScript with Knex backend, NextJS frontend
                         </p>
-                        <p>
+                        <p className="pl-12">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio harum accusantium excepturi, vel vitae maiores nemo ea, ab sint eos totam exercitationem non tempora blanditiis placeat rerum voluptatibus architecto libero.
                             Quaerat ab natus excepturi eos at, veniam alias nulla, commodi, recusandae voluptatum magnam optio voluptatem ipsum nesciunt perspiciatis reiciendis iusto. Quam enim beatae quo ea ducimus incidunt temporibus accusantium doloribus!
                             Iusto voluptate pariatur dolorem facere reprehenderit nihil numquam nostrum ad cumque est laudantium vero corporis dolore, praesentium atque ducimus. Non quo itaque alias magni, odit reprehenderit quod iusto earum voluptatibus?
                             Temporibus, aliquam odit. Quidem minus nesciunt aspernatur qui nihil. Aperiam amet dolor commodi, nostrum hic dolorum. Quos dolor consequatur officiis libero neque mollitia ex voluptatum, fuga, facilis quod sunt atque.
                             Quas, itaque ut deleniti corporis sunt illum ipsam hic eos obcaecati cum praesentium labore quibusdam possimus! Ratione consequuntur amet quis commodi enim eum a nam. Ab provident voluptatum nisi cupiditate!
                         </p>
+
+                        <Link to="https://github.com/saber-quest" className="btn-secondary p-4 rounded-l text-black mt-2">Project Source</Link>
                     </div>
                 </section>
             </div>
