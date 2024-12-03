3844f05f2f364903e77ea4ff56001b10e9f4e864
Raine Aeternal <64136899+raineaeternal@users.noreply.github.com>
2024-12-03 09:33:01 +0100

    fix unused import in ffxiv.tsx

--- src/pages/ffxiv.tsx
+++ src/pages/ffxiv.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
 export default function FFXIV() {
     // Get data from FFLogs, but their API is confusing, so this is postponed until further notice.
