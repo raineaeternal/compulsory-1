780fc817f0fe949f25a6b942bdedec1311dee9fa
Raine <64136899+raineaeternal@users.noreply.github.com>
2025-01-01 01:04:24 +0100

    Update env.ts

--- src/env.ts
+++ src/env.ts
@@ -1,18 +1,20 @@
 import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
+const processEnv: { [key: string]: string | undefined } = {
+  NODE_ENV: process.env.NODE_ENV,
+};
+
 export const env = createEnv({
- server: {
-   NODE_ENV: z.enum(["development", "test", "production"]),
- },
- client: {},
- runtimeEnv: {
-   NODE_ENV: process.env.NODE_ENV,
- },
- skipValidation: !!process.env.SKIP_ENV_VALIDATION,
- emptyStringAsUndefined: true,
- isServer: typeof window === "undefined",
- experimental__runtimeEnv: {
-   NODE_ENV: process.env.NODE_ENV,
- }
+  server: {
+    NODE_ENV: z.enum(["development", "test", "production"]),
+  },
+  client: {},
+  runtimeEnv: processEnv,
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+  emptyStringAsUndefined: true,
+  isServer: typeof window === "undefined",
+  experimental__runtimeEnv: processEnv
 });
+
+export type Env = typeof env;
